6dcc38612eec3e867defe3c9d6b269aae6bf3c98
nullren <chopflop@gmail.com>
2013-12-06 02:06:41 -0800

    plots my graphs and i have user logins, so i think we are in alpha 1

--- client/client.js
+++ client/client.js
@@ -13,64 +13,69 @@ Template.list.pressures = function() {
 };
 
 Template.chart.rendered = function() {
-  //var self = this;
+  var self = this;
+  self.node = this.find('svg');
 
-  // graph things
-  var w = 500;
-  var h = 500;
-  var margin = 20;
+  if (! self.handle) {
+    self.handle = Deps.autorun(function() {
+      // graph things
+      var w = 500;
+      var h = 500;
+      var margin = 20;
 
-  // data things
-  var data = Pressures.find({}, {sort: {date: -1}});
-  var dias = data.map(function(d){ return d.dia; });
-  var syss = data.map(function(d){ return d.sys; });
-  var puls = data.map(function(d){ return d.pulse; });
+      // data things
+      var data = function() { return Pressures.find({}, {sort: {date: 1}}); };
+      var syss = data().map(function(d){ return d.sys; });
+      var dias = data().map(function(d){ return d.dia; });
+      var puls = data().map(function(d){ return d.pulse; });
 
-  // max
-  var max = d3.max(dias);
-  var max_s = d3.max(syss);
-  var max_p = d3.max(puls);
+      // max
+      var max = d3.max(dias);
 
-  // set the scales
-  var x = d3.scale.linear().domain([0, dias.length]).range([0 + margin, w - margin]);
-  var y = d3.scale.linear().domain([max, 0]).range([0 + margin, h - margin]);
+      // set the scales
+      var x = d3.scale.linear().domain([0, dias.length]).range([0 + margin, w - margin]);
+      var y = d3.scale.linear().domain([max, 0]).range([0 + margin, h - margin]);
 
-  // base vis layer
-  var vis = d3.select('#chart')
-      .append('svg:svg')
-      .attr('width', w)
-      .attr('height', h);
+      // base vis layer
+      d3.select('#chart svg').remove();
+      var vis = d3.select('#chart')
+          .append('svg')
+          .attr('width', w)
+          .attr('height', h);
 
-  // creates a group element
-  var g = vis.append('svg:g');
+      // creates a group element
+      var g = vis.append('svg:g');
 
-  var line = d3.svg.line()
-      .x(function(d,i) { return x(i); })
-      .y(function(d) { return y(d); });
+      var line = d3.svg.line()
+          .x(function(d,i) { return x(i); })
+          .y(function(d) { return y(d); });
 
-  // creates the line
-  var make_path = function(d, color) {
-    g.append('svg:path')
-      .attr('stroke', color)
-      .attr('stroke-width', '2')
-      .attr('fill', 'none')
-      .attr('d', line(d));
-  };
-  make_path(dias, 'green');
+      // creates the line
+      var make_path = function(d, color) {
+        g.append('svg:path')
+          .attr('stroke', color)
+          .attr('stroke-width', '2')
+          .attr('fill', 'none')
+          .attr('d', line(d));
+      };
+      make_path(dias, 'blue');
+      make_path(syss, 'red');
+      make_path(puls, 'green');
 
-  // creates axes
-  g.append('svg:line')
-    .attr('stroke', 'black')
-    .attr('x1', x(0))
-    .attr('y1', y(0))
-    .attr('x2', x(w))
-    .attr('y2', y(0));
+      // creates axes
+      g.append('svg:line')
+        .attr('stroke', 'black')
+        .attr('x1', x(0))
+        .attr('y1', y(0))
+        .attr('x2', x(w))
+        .attr('y2', y(0));
 
-  g.append('svg:line')
-    .attr('stroke', 'black')
-    .attr('x1', x(0))
-    .attr('y1', y(0))
-    .attr('x2', x(0))
-    .attr('y2', y(d3.max(dias)));
-
+      g.append('svg:line')
+        .attr('stroke', 'black')
+        .attr('x1', x(0))
+        .attr('y1', y(0))
+        .attr('x2', x(0))
+        .attr('y2', y(d3.max(dias)));
+    });
+  }
 };
